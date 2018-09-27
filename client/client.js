e7abdcb7c1965e8b249fd5f00c4dda36e7e413e5
T. Andrew Taton <thomas.andrew.taton@gmail.com>
2018-09-26 19:24:06 -0500

    Sockets!

--- client/client.js
+++ client/client.js
@@ -1,4 +1,5 @@
 const socket = io();
+Big.DP = 10;
 
 let calculation = [];
 // Odd-indexed array elements are numberStrings; even-index elements are operators.
@@ -7,13 +8,13 @@ let prevResult = initialValue;
 let currentValue = initialValue;
 
 // Output elements
+const nameEntryEl = document.getElementById('name-entry');
 const currentValueEl = document.getElementById('current-value');
-const currentCalcEl = document.getElementById('current-calc');
-const calcReport = document.getElementById('calc-report');
+const currentCalcEl = document.getElementById('calc-content');
+const calcReportEl = document.getElementById('calc-report');
 
 const addDigit = digit => {
   currentValue = currentValue === '0' ? digit : currentValue + digit;
-  console.log(currentValue);
 };
 
 const clearValue = () => {
@@ -30,7 +31,7 @@ const addCalc = (value, operatorString) => {
   clearValue();
 };
 
-const clearCalc = () => {
+const clearAll = () => {
   calculation = [];
   clearPrevResult();
   clearValue();
@@ -62,10 +63,10 @@ const addOperator = operatorString => {
 const parseCalcArray = () =>
   calculation
     .join(' ')
-    .replace('add', '+')
+    .replace('plus', '+')
     .replace('minus', '-')
-    .replace('multiply', '&times;')
-    .replace('divide', '&divide;');
+    .replace('times', '&times;')
+    .replace('div', '&divide;');
 
 // Simple button event handlers:
 document.querySelectorAll('button.digit').forEach(element => {
@@ -78,7 +79,7 @@ document.querySelectorAll('button.operator').forEach(element => {
     addOperator(element.name);
   });
 });
-document.getElementById('clear-all').addEventListener('click', clearCalc);
+document.getElementById('clear-all').addEventListener('click', clearAll);
 document.getElementById('clear-display').addEventListener('click', clearValue);
 document.getElementById('plus-minus').addEventListener('click', () => {
   if (currentValue.length > 0 && currentValue !== '0') {
@@ -98,13 +99,34 @@ document.getElementById('decimal-point').addEventListener('click', () => {
   } // else decimal point has already been added; ignore
 });
 
+const subCalc = (big1, operator, big2) => big1[operator](big2);
+
+const calcReduce = operator => {
+  while (calculation.includes(operator)) {
+    const calcPoint = calculation.indexOf(operator);
+    calculation = [
+      ...calculation.slice(0, calcPoint - 1),
+      subCalc(...calculation.slice(calcPoint - 1, calcPoint + 2)),
+      ...calculation.slice(calcPoint + 2)
+    ];
+  }
+};
+
 // Calculate function.
 const calculate = () => {
   const calcString = parseCalcArray();
   for (let i = 0; i < calculation.length; i += 2) {
     calculation[i] = new Big(calculation[i]);
   }
-  while (calculation.length > 1) {}
+  ['times', 'div', 'plus', 'minus'].forEach(operator => calcReduce(operator));
+  const result = calculation[0].toString();
+  socket.emit('calculation', {
+    calculation: calcString + ' = ' + result,
+    name: nameEntryEl.value || 'Anonymous user'
+  });
+  prevResult = result;
+  clearValue();
+  calculation = [];
 };
 
 // Equals button triggers calculate().
@@ -120,10 +142,27 @@ document.getElementById('equals').addEventListener('click', () => {
   }
 });
 
+const updateDisplay = () => {
+  currentValueEl.textContent = currentValue
+    ? currentValue
+    : prevResult
+      ? prevResult
+      : '0';
+  currentCalcEl.innerHTML = parseCalcArray();
+};
+
 // Additional button handler to refresh display values
 document.querySelectorAll('button.btn').forEach(element => {
-  element.addEventListener('click', () => {
-    currentValueEl.textContent = currentValue;
-    currentCalcEl.innerHTML = parseCalcArray();
+  element.addEventListener('click', updateDisplay);
+});
+
+updateDisplay();
+
+socket.on('report', calcArray => {
+  calcReportEl.innerHTML = '';
+  calcArray.forEach(({ calculation, name }) => {
+    const listEl = document.createElement('li');
+    listEl.innerHTML = name + ' calculated: ' + calculation;
+    calcReportEl.insertAdjacentElement('beforeend', listEl);
   });
 });
--- server/server.js
+++ server/server.js
@@ -2,33 +2,16 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-const session = require('express-session')({
-  secret: 'socket-secret',
-  resave: true,
-  saveUninitialized: true
-});
-const sharedsession = require('express-socket.io-session');
+let calcArray = [];
+
 const PORT = process.env.PORT || 3000;
 
-// Attach session to express
-app.use(session);
-
-// Share express session with socket.io
-io.use(sharedsession(session));
-
 io.on('connection', socket => {
-  console.log('New socket id: ' + socket.id);
-  // Accept a login event with user's data
-  // socket.on('login', function(userdata) {
-  //   socket.handshake.session.userdata = userdata;
-  //   socket.handshake.session.save();
-  // });
-  // socket.on('logout', function(userdata) {
-  //   if (socket.handshake.session.userdata) {
-  //     delete socket.handshake.session.userdata;
-  //     socket.handshake.session.save();
-  //   }
-  // });
+  socket.emit('report', calcArray);
+  socket.on('calculation', calcObject => {
+    calcArray = [calcObject, ...calcArray.slice(0, 9)];
+    io.emit('report', calcArray);
+  });
 });
 
 // Serve static files
